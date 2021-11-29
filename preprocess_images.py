613b37414768a8e0b975586d53260c9c0672061b
vinizm <vmartins@poli.ufrj.br>
2021-11-28 21:39:24 -0300

    preprocess_images

---
+++ preprocess_images.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+from utils.utils import load_images, extract_patches, extract_patches_in_batches, save_arrays, convert_to_onehot_tensor
+
+
+STRIDE_TRAIN = 256
+STRIDE_TEST = 512
+NUM_CLASS = 2
+
+PATH_TO_FE19_DATASET_RLM = 
+PATH_TO_FE19_DATASET_MASK = 
+
+
+images_rlm = load_images(path_to_folder = PATH_TO_FE19_DATASET_RLM, normalize = True, one_channel = True)
+images_ref = load_images(path_to_folder = PATH_TO_FE19_DATASET_MASK, normalize = True, one_channel = True)
+
+patches_rlm_train = extract_patches_in_batches(images = images_rlm, patch_size = 512, stride = STRIDE_TRAIN)
+patches_ref_train = extract_patches_in_batches(images = images_ref, patch_size = 512, stride = STRIDE_TRAIN)
+
+patches_rlm_test = extract_patches_in_batches(images = images_rlm, patch_size = 512, stride = STRIDE_TEST)
+patches_ref_test = extract_patches_in_batches(images = images_ref, patch_size = 512, stride = STRIDE_TEST)
+
+patches_ref_train_onehot = convert_to_onehot_tensor(tensor = patches_ref_train, num_class = NUM_CLASS)
+patches_train = np.concatenate((patches_rlm_train, patches_ref_train_onehot), axis = 3)
+
+patches_ref_test_onehot = convert_to_onehot_tensor(tensor = patches_ref_test, num_class = NUM_CLASS)
+patches_test = np.concatenate((patches_rlm_test, patches_ref_test_onehot), axis = 3)
+
+save_arrays(patches_train, f'./Processed/Fe19_stride512_Train/', suffix = '', ext = '.npy')
+save_arrays(patches_test, f'./Processed/Fe19_stride512_Test/', suffix = '', ext = '.npy')
