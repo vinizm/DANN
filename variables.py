cf152f6e5b9c90e00706d5a99e6aae955f8ba84d
vinizm <vmartins@poli.ufrj.br>
2021-12-04 00:44:29 +0000

    changing path to linux

--- variables.py
+++ variables.py
@@ -1,16 +1,14 @@
 
 
-BASE_FOLDER = 'C:/Users/Vinicius Martins/Documents/CCOMP/Projeto'
-DATASET_FOLDER = f'{BASE_FOLDER}/Datasets'
-PROCESSED_FOLDER = f'{BASE_FOLDER}/code/DANN/processed_images'
+BASE_FOLDER = '/root'
+PROCESSED_FOLDER = f'{BASE_FOLDER}/DANN/processed_images'
 
-PATH_TO_FE19_DATASET_RLM = f'{DATASET_FOLDER}/Fe19/Reflected_Light_Microscopy'
-PATH_TO_FE120_DATASET_RLM = f'{DATASET_FOLDER}/Fe120/Reflected_Light_Microscopy'
-PATH_TO_FEM_DATASET_RLM = f'{DATASET_FOLDER}/FeM/Reflected_Light_Microscopy'
-PATH_TO_CU_DATASET_RLM = f'{DATASET_FOLDER}/Cu/Reflected_Light_Microscopy'
+PATH_TO_FE19_DATASET_RLM = f'{BASE_FOLDER}/Fe19/Reflected_Light_Microscopy'
+PATH_TO_FE120_DATASET_RLM = f'{BASE_FOLDER}/Fe120/Reflected_Light_Microscopy'
+PATH_TO_FEM_DATASET_RLM = f'{BASE_FOLDER}/FeM/Reflected_Light_Microscopy'
+PATH_TO_CU_DATASET_RLM = f'{BASE_FOLDER}/Cu/Reflected_Light_Microscopy'
 
-PATH_TO_FE19_DATASET_MASK = f'{DATASET_FOLDER}/Fe19/Reference_1'
-PATH_TO_FE120_DATASET_MASK = f'{DATASET_FOLDER}/Fe120/Reference'
-PATH_TO_FEM_DATASET_MASK = f'{DATASET_FOLDER}/FeM/Reference'
-PATH_TO_CU_DATASET_MASK = f'{DATASET_FOLDER}/Cu/Reference'
-
+PATH_TO_FE19_DATASET_MASK = f'{BASE_FOLDER}/Fe19/Reference_1'
+PATH_TO_FE120_DATASET_MASK = f'{BASE_FOLDER}/Fe120/Reference'
+PATH_TO_FEM_DATASET_MASK = f'{BASE_FOLDER}/FeM/Reference'
+PATH_TO_CU_DATASET_MASK = f'{BASE_FOLDER}/Cu/Reference'
