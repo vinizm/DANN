2a47f51b0fa5cf2d1a32689e6dbfb3f4aead4da0
vinizm <vmartins@poli.ufrj.br>
2021-12-12 22:34:49 -0300

    return

--- testing.py
+++ testing.py
@@ -23,27 +23,27 @@ def Test(model, test_dir: str, num_images_test: int, path_to_metrics: str, chann
 
 	print('Predicting...')
 	y_pred_total = model.predict(x_test_total)
-	print(y_pred_total.shape)
+
 	return y_pred_total, y_test_total
 
-	# metrics = compute_metrics(y_test_total.flatten(), y_pred_total.flatten())
+	metrics = compute_metrics(y_test_total.flatten(), y_pred_total.flatten())
 
-	# accuracy = metrics.get('accuracy')
-	# print(f'Overall accuracy (number of correctly predicted items/total of item to predict): {accuracy}')
+	accuracy = metrics.get('accuracy')
+	print(f'Overall accuracy (number of correctly predicted items/total of item to predict): {accuracy}')
 
-	# avg_precision = metrics.get('average_precision')
-	# print(f'Average accuracy (the average of each accuracy per class(sum of accuracy for each class predicted/number of class)): {avg_precision}')
+	avg_precision = metrics.get('average_precision')
+	print(f'Average accuracy (the average of each accuracy per class(sum of accuracy for each class predicted/number of class)): {avg_precision}')
 
-	# precision = metrics.get('precision')
-	# print(f'Precision (how many of them are actual positive): {precision}')
+	precision = metrics.get('precision')
+	print(f'Precision (how many of them are actual positive): {precision}')
 
-	# recall = metrics.get('recall')
-	# print(f'Recall (how many of the actual Positives our model capture through labeling it as Positive (True Positive)): {recall}')
+	recall = metrics.get('recall')
+	print(f'Recall (how many of the actual Positives our model capture through labeling it as Positive (True Positive)): {recall}')
 
-	# f1 = metrics.get('f1_score')
-	# print(f'F1 score: {f1}')
+	f1 = metrics.get('f1_score')
+	print(f'F1 score: {f1}')
 
-	# save_json(metrics, path_to_metrics)
+	save_json(metrics, path_to_metrics)
 
 
 def Test_Case(test_dir: str, num_images_test: int, path_to_metrics: str, channels: int, is_model: bool = False,
@@ -60,7 +60,7 @@ def Test_Case(test_dir: str, num_images_test: int, path_to_metrics: str, channel
 							  alpha = 1., activation = 'sigmoid')
 		model.load_weights(path_to_load)
 
-	Test(model = model, test_dir = test_dir, num_images_test = num_images_test, path_to_metrics = path_to_metrics, channels = channels)
+	return Test(model = model, test_dir = test_dir, num_images_test = num_images_test, path_to_metrics = path_to_metrics, channels = channels)
 
 
 if __name__ == '__main__':
