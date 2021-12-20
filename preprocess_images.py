c2f904287761cbd52eed18847ab7a05697242503
vinizm <vmartins@poli.ufrj.br>
2021-12-20 12:08:16 -0300

    set specific test images

--- preprocess_images.py
+++ preprocess_images.py
@@ -1,26 +1,37 @@
 import sys
+import os
 import numpy as np
 
 from utils.utils import load_images, extract_patches_from_images, save_arrays, convert_to_onehot_tensor
 from variables import PATH_TO_FE19_DATASET_RLM, PATH_TO_FE19_DATASET_MASK
 
 if __name__ == '__main__':
-    ONE_CHANNEL = eval(sys.argv[1]) # one channel boolean
+    # ONE_CHANNEL = eval(sys.argv[1]) # one channel boolean
+    ONE_CHANNEL = True
     print(f'one_channel: {ONE_CHANNEL}')
 
     PATCH_SIZE = 512
     STRIDE_TRAIN = 256
     STRIDE_TEST = 512
     NUM_CLASS = 2
+    TEST_INDEX = [1, 5, 11, 17]
 
     images_rlm = load_images(path_to_folder = PATH_TO_FE19_DATASET_RLM, normalize = True, one_channel = ONE_CHANNEL)
     images_ref = load_images(path_to_folder = PATH_TO_FE19_DATASET_MASK, normalize = True, one_channel = ONE_CHANNEL)
 
-    patches_rlm_train = extract_patches_from_images(images = images_rlm, patch_size = PATCH_SIZE, stride = STRIDE_TRAIN)
-    patches_ref_train = extract_patches_from_images(images = images_ref, patch_size = PATCH_SIZE, stride = STRIDE_TRAIN)
+    train_index = [i for i in range(len(images_ref)) if i not in TEST_INDEX]
 
-    patches_rlm_test = extract_patches_from_images(images = images_rlm, patch_size = PATCH_SIZE, stride = STRIDE_TEST)
-    patches_ref_test = extract_patches_from_images(images = images_ref, patch_size = PATCH_SIZE, stride = STRIDE_TEST)
+    images_rlm_train = [images_rlm[i] for i in train_index]
+    images_ref_train = [images_ref[i] for i in train_index]
+
+    images_rlm_test = [images_rlm[i] for i in TEST_INDEX]
+    images_ref_test = [images_ref[i] for i in TEST_INDEX]
+
+    patches_rlm_train = extract_patches_from_images(images = images_rlm_train, patch_size = PATCH_SIZE, stride = STRIDE_TRAIN)
+    patches_ref_train = extract_patches_from_images(images = images_ref_train, patch_size = PATCH_SIZE, stride = STRIDE_TRAIN)
+
+    patches_rlm_test = extract_patches_from_images(images = images_rlm_test, patch_size = PATCH_SIZE, stride = STRIDE_TEST)
+    patches_ref_test = extract_patches_from_images(images = images_ref_test, patch_size = PATCH_SIZE, stride = STRIDE_TEST)
 
     patches_ref_train_onehot = convert_to_onehot_tensor(tensor = patches_ref_train, num_class = NUM_CLASS)
     patches_train = np.concatenate((patches_rlm_train, patches_ref_train_onehot), axis = 3)
