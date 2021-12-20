ea5e20e26a08fc1c0214b89a5f78dbdc4cb76ebc
vinizm <vmartins@poli.ufrj.br>
2021-12-20 12:33:02 -0300

    predicting in batches

--- testing.py
+++ testing.py
@@ -8,37 +8,49 @@ from model import Deeplabv3plus
 from variables import *
 
 
-def Test(model, test_dir: str, num_images_test: int, path_to_metrics: str, channels: int):
+def Test(model, test_dir: str, num_images_test: int, path_to_metrics: str, channels: int, batch_size: int):
 	print('Start testing...')
 	
 	test_data_dirs = glob.glob(test_dir + '/*.npy')
-	np.random.shuffle(test_data_dirs)
+	# np.random.shuffle(test_data_dirs)
 	test_data_dirs = test_data_dirs[: num_images_test]
 
-	batch_images = np.asarray([load_array(batch_file) for batch_file in test_data_dirs])
-	batch_images = np.float32(batch_images) # set np.float32 to reduce memory usage
+	# compute number of batches
+	num_batches = len(test_data_dirs) // batch_size
 
-	x_test_total = batch_images[ :, :, :, : channels]
-	y_test_total = batch_images[ :, :, :, channels :]
+	final_test = []
+	final_pred = []
+	for batch in range(num_batches):
+		print(f'Batch {batch + 1} of {num_batches}')
+		batch_files = test_data_dirs[batch * batch_size : (batch + 1) * batch_size]
 
-	print('Predicting...')
-	y_pred_total = model.predict(x_test_total)
+		batch_images = np.asarray([load_array(batch_file) for batch_file in batch_files])
+		batch_images = np.float32(batch_images) # set np.float32 to reduce memory usage
 
-	return y_test_total, y_pred_total
+		x_test_total = batch_images[ :, :, :, : channels]
+		y_test_total = batch_images[ :, :, :, channels :]
+
+		print('Predicting...')
+		y_pred_total = model.predict(x_test_total)
+
+		final_test.append(y_test_total)
+		final_pred.append(y_pred_total)
+
+	return np.asarray(final_test), np.asarray(final_pred)
 
 	# metrics = compute_metrics(y_test_total.reshape(-1), np.round(y_pred_total).reshape(-1))
 
 	# accuracy = metrics.get('accuracy')
-	# print(f'Overall accuracy: {accuracy}')
+	# print(f'overall accuracy: {accuracy}')
 
 	# avg_precision = metrics.get('average_precision')
-	# print(f'Average precision: {avg_precision}')
+	# print(f'average precision: {avg_precision}')
 
 	# precision = metrics.get('precision')
-	# print(f'Precision: {precision}')
+	# print(f'precision: {precision}')
 
 	# recall = metrics.get('recall')
-	# print(f'Recall: {recall}')
+	# print(f'recall: {recall}')
 
 	# f1 = metrics.get('f1_score')
 	# print(f'F1 score: {f1}')
@@ -46,8 +58,9 @@ def Test(model, test_dir: str, num_images_test: int, path_to_metrics: str, chann
 	# save_json(metrics, path_to_metrics)
 
 
-def Test_Case(test_dir: str, num_images_test: int, path_to_metrics: str, channels: int, is_model: bool = False,
-			  patch_size: int = None, num_class: int = None, output_stride: int = None, path_to_load: str = None):
+def Test_Case(test_dir: str, num_images_test: int, path_to_metrics: str, channels: int, batch_size: int,
+			  is_model: bool = False, patch_size: int = None, num_class: int = None, output_stride: int = None,
+			  path_to_load: str = None):
 
 	if is_model: # load model
 		print('Loading model.')
@@ -60,7 +73,8 @@ def Test_Case(test_dir: str, num_images_test: int, path_to_metrics: str, channel
 							  alpha = 1., activation = 'sigmoid')
 		model.load_weights(path_to_load)
 
-	return Test(model = model, test_dir = test_dir, num_images_test = num_images_test, path_to_metrics = path_to_metrics, channels = channels)
+	return Test(model = model, test_dir = test_dir, num_images_test = num_images_test, path_to_metrics = path_to_metrics,
+				channels = channels, batch_size = batch_size)
 
 
 if __name__ == '__main__':
@@ -75,8 +89,9 @@ if __name__ == '__main__':
 	patch_size = 512
 	num_class = 2
 	output_stride = 8
+	batch_size = 2
 	num_images_test = None
 
 	Test_Case(test_dir = test_dir, num_images_test = num_images_test, path_to_metrics = path_to_metrics,
 			  channels = channels, is_model = is_model, patch_size = patch_size, num_class = num_class,
-			  output_stride = output_stride, path_to_load = None)
+			  output_stride = output_stride, path_to_load = None, batch_size = batch_size)
