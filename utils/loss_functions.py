ae8a28b5c11759fdde226c4357bc3528469df957
vinizm <vmartins@poli.ufrj.br>
2022-01-12 22:50:54 -0300

    implementation of binary_crossentropy

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2
+import tensorflow as tf
 
 
 def pixel_wise_weighted_loss(y_true, y_pred):
     pass
 
+
+def binary_crossentropy(y_true, y_pred):
+    tmp = tf.math.multiply(y_true, y_pred)
+    tmp = tf.reduce_sum(tmp, axis = -1)
+    loss = -1. * tf.math.log(tmp)
+    return loss
