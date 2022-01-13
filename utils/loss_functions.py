61ebadde9594c9bf30d5950d652759844bb0a819
vinizm <vmartins@poli.ufrj.br>
2022-01-12 23:45:17 -0300

    implementation of pixel_wise_d1_weighted_loss

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -2,9 +2,17 @@ import numpy as np
 import cv2
 import tensorflow as tf
 
+from variables import EPSILON
+from utils.utils import generate_weight_maps
 
-def pixel_wise_weighted_loss(y_true, y_pred):
-    pass
+
+def pixel_wise_d1_weighted_loss(y_true, y_pred):
+    wmaps = generate_weight_maps(y_true, EPSILON)
+
+    tmp = tf.math.multiply(y_true, y_pred)
+    tmp = tf.reduce_sum(tmp, axis = -1)
+    loss = -1. * tf.math.multiply(wmaps, tf.math.log(tmp))
+    return loss
 
 
 def binary_crossentropy(y_true, y_pred):
