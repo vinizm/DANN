2f59c7bf4b3a8e72946d2edd8e9f41bf02254da5
vinizm <vmartins@poli.ufrj.br>
2022-01-13 21:35:07 -0300

    removing comments

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -6,28 +6,18 @@ from variables import EPSILON
 from utils.utils import generate_weight_maps
 
 
-class WeightedD1BinaryCrossEntropy(tf.keras.losses.Loss):
-
-    def call(self, y_true, y_pred):
-        wmaps = generate_weight_maps(y_true, EPSILON)
-
-        tmp = tf.math.multiply(y_true, y_pred)
-        tmp = tf.reduce_sum(tmp, axis = -1)
-        loss = -1. * tf.math.multiply(wmaps, tf.math.log(tmp))
-        return loss
-
 # def weighted_d1_binary_crossentropy(y_true, y_pred):
-# def binary_crossentropy(y_true, y_pred):
-#     wmaps = generate_weight_maps(y_true, EPSILON)
+def binary_crossentropy(y_true, y_pred):
+    wmaps = generate_weight_maps(y_true, EPSILON)
 
-#     tmp = tf.math.multiply(y_true, y_pred)
-#     tmp = tf.reduce_sum(tmp, axis = -1)
-#     loss = -1. * tf.math.multiply(wmaps, tf.math.log(tmp))
-#     return loss
+    tmp = tf.math.multiply(y_true, y_pred)
+    tmp = tf.reduce_sum(tmp, axis = -1)
+    loss = -1. * tf.math.multiply(wmaps, tf.math.log(tmp))
+    return loss
 
 
-# def binary_crossentropy_x(y_true, y_pred):
-#     tmp = tf.math.multiply(y_true, y_pred)
-#     tmp = tf.reduce_sum(tmp, axis = -1)
-#     loss = -1. * tf.math.log(tmp)
-#     return loss
+def binary_crossentropy_x(y_true, y_pred):
+    tmp = tf.math.multiply(y_true, y_pred)
+    tmp = tf.reduce_sum(tmp, axis = -1)
+    loss = -1. * tf.math.log(tmp)
+    return loss
