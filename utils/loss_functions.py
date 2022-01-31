0c39f5f1bca9e00983bdd231a15ebe52c005b17f
vinizm <vmartins@poli.ufrj.br>
2022-01-30 21:02:16 -0300

    defining weighted_d1_binary_crossentropy as class

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import tensorflow as tf
+from tensorflow.keras.losses import Loss
 
 from variables import EPSILON
 from utils.utils import generate_weight_maps
@@ -21,3 +22,18 @@ def binary_crossentropy_x(y_true, y_pred):
     tmp = tf.reduce_sum(tmp, axis = -1)
     loss = -1. * tf.math.log(tmp)
     return loss
+
+
+class weighted_d1_binary_crossentropy(Loss):
+
+    def __init__(self, epsilon: float):
+        super().__init__()
+        self.epsilon = epsilon
+
+    def call(self, y_true, y_pred):
+        wmaps = generate_weight_maps(y_true, self.epsilon)
+
+        tmp = tf.math.multiply(y_true, y_pred)
+        tmp = tf.reduce_sum(tmp, axis = -1)
+        loss = -1. * tf.math.multiply(wmaps, tf.math.log(tmp))
+        return loss
