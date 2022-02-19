0f135d2787472a100bfc13db2091493601a870bb
vinizm <vmartins@poli.ufrj.br>
2022-02-19 16:58:28 -0300

    plot_map

--- plot.py
+++ plot.py
@@ -1,4 +1,17 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 
 
+
+def plot_map(wmap: np.ndarray):
+    fig = plt.figure(1, figsize = (7, 7))
+
+    ax = plt.gca()
+    im = ax.imshow(wmap, cmap = 'jet')
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size = '5%', pad = 0.1)
+
+    plt.colorbar(im, cax = cax)
+    ax.axis('off')
