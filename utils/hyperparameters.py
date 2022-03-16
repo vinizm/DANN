049ecb1f2192b91d479272c601a45a72f5f8d846
vinizm <vmartins@poli.ufrj.br>
2022-03-15 21:53:07 -0300

    script for hyperparameters

---
+++ utils/hyperparameters.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from config import *
+
+
+LR_WARMUP = 0.4
+LAMBDA_WARMUP = 0.2
+LR0 = 5e-4
+ALPHA = 5.
+BETA = 0.75
+GAMMA = 10.
+
+def learning_rate_decay(p: float):
+	return LR0 / ((1 + ALPHA * (p - LR_WARMUP)) ** BETA)
+
+def lambda_grl(p: float):
+	return (2 / (1 + np.exp(- GAMMA * p))) - 1
