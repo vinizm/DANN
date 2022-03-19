c22f6135cdc3cb1616b3d3f0c25bebf2b18dfe23
vinizm <vmartins@poli.ufrj.br>
2022-03-19 00:52:06 -0300

    removing useless import

--- model_builder.py
+++ model_builder.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from cv2 import flip
 
 import tensorflow as tf
 from tensorflow.keras.models import Model
