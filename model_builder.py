4a4dc99aa63c4c764a15bfbdbb04924fd2a38985
vinizm <vmartins@poli.ufrj.br>
2022-03-20 19:41:39 -0300

    syntax error

--- model_builder.py
+++ model_builder.py
@@ -32,7 +32,7 @@ class GradientReversalLayer(Layer):
     
     def call(self, inputs):
         x, l_tensor = inputs
-        l = float(l_tensor[0])]
+        l = float(l_tensor[0])
         print(l)
         y = self.flipper(x, l)
         return y
