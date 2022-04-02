76864e8b18c5f2e2e9d95e0879ef4f905111c25f
vinizm <vmartins@poli.ufrj.br>
2022-04-01 22:33:14 -0300

    learning rate factory

--- utils/learning_rate_functions.py
+++ utils/learning_rate_functions.py
@@ -1,6 +1,7 @@
+import numpy as np
 from abc import ABC, abstractmethod
 
-from utils.hyperparameters import LR_WARMUP, LR0, ALPHA, BETA
+from utils.hyperparameters import LR_WARMUP, LR0, ALPHA, BETA, STEP_DECAY, NUM_STEPS
 
 
 class AbstractLearningRate(ABC):
@@ -21,7 +22,7 @@ class LearningRateConstant(AbstractLearningRate):
     def calculate(self, p: float):
         return self.const
 
-class LearningRateDecay(AbstractLearningRate):
+class LearningRateExpDecay(AbstractLearningRate):
 
     def __init__(self, lr0 = LR0, lr_warmup = LR_WARMUP, alpha = ALPHA, beta = BETA):
         self.lr0 = lr0
@@ -34,3 +35,32 @@ class LearningRateDecay(AbstractLearningRate):
             return self.lr0
         return self.lr0 / ((1 + self.alpha * (p - self.lr_warmup)) ** self.beta)
 
+class LearningRateStepDecay(AbstractLearningRate):
+
+    def __init__(self, lr0 = LR0, decay = STEP_DECAY, num_steps = NUM_STEPS):
+        self.lr0 = lr0
+        self.decay = decay
+        self.num_steps = num_steps
+
+        self.points = np.linspace(0, 1, self.num_steps + 1)
+
+    def calculate(self, p: float):
+        
+        for i in range(len(self.points) - 1):
+            if p < self.points[i + 1]:
+
+                return self.lr0 / (self.decay ** (i - 1))
+
+class LearningRateFactory:
+
+    @staticmethod
+    def get_function(name: str, **kargs):
+
+        if name == 'constant':
+            return LearningRateConstant(**kargs)
+
+        elif name == 'exponential':
+            return LearningRateExpDecay(**kargs)
+
+        elif name == 'step':
+            return LearningRateStepDecay(**kargs)
