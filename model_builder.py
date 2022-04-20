d7341670780b1bc42c22835bd4da75e72ea177de
vinizm <vmartins@poli.ufrj.br>
2022-04-19 21:00:45 -0300

    add batch normalization

--- model_builder.py
+++ model_builder.py
@@ -95,8 +95,8 @@ class DomainDiscriminator(Model):
         super(DomainDiscriminator, self).__init__(**kwargs)
         self.units = units
 
+        self.batch_norm_1 = BatchNormalization(axis = -1, epsilon = 1.e-32)
         self.flat = Flatten()
-        #self.batch_norm_1 = BatchNormalization(scale = False, center = False, axis = -1, epsilon = 1.e-20)
         self.dense_1 = Dense(units = units)
         self.activ_1 = Activation('relu')
         self.dense_2 = Dense(units = units)
@@ -106,8 +106,8 @@ class DomainDiscriminator(Model):
 
     def call(self, inputs):
 
-        x = self.flat(inputs)
-        #x = self.batch_norm_1(x, training = True)
+        x = self.batch_norm_1(inputs)
+        x = self.flat(x)
         x = self.dense_1(x)
         x = self.activ_1(x)
         x = self.dense_2(x)
