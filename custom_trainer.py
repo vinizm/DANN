b570a75e4b8bda94372e5179a6bd850d48218b74
vinizm <vmartins@poli.ufrj.br>
2022-04-23 22:50:31 -0300

    no major changes

--- custom_trainer.py
+++ custom_trainer.py
@@ -36,7 +36,7 @@ class Trainer():
 		self.lr_function_discriminator = lr_factory.get_function('constant', const = 1.e-3)
 
 		self.lr_function_segmentation = lr_factory.get_function('exp_decay', lr0 = LR0, warmup = 0.1, alpha = 10., beta = 0.75)
-		# self.lr_function_discriminator = lr_factory.get_function('exp_decay', lr0 = LR0, warmup = 0.12, alpha = 10., beta = 0.75)
+		# self.lr_function_discriminator = lr_factory.get_function('exp_decay', lr0 = LR0, warmup = 0.1, alpha = 10., beta = 0.75)
 
 		self.lambda_function = LambdaGradientReversalLayer(warmup = 0.25, gamma = 10., lambda_scale = 0.1)
 
