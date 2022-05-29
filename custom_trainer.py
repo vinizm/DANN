1c73c76d458041844e639caba100341cb34a3bec
vinizm <vmartins@poli.ufrj.br>
2022-05-29 17:36:11 -0300

    loging losses

--- custom_trainer.py
+++ custom_trainer.py
@@ -566,7 +566,11 @@ class Trainer():
 			self.acc_segmentation_train_history.append(acc_segmentation_train)
 			
 			acc_discriminator_train = float(self.acc_function_discriminator.result())
-			self.acc_discriminator_train_history.append(acc_discriminator_train)			
+			self.acc_discriminator_train_history.append(acc_discriminator_train)
+
+			with self.train_writer.as_default():
+				tf.summary.scalar('loss_segmentation', loss_segmentation_train, step = epoch + 1)
+				tf.summary.scalar('loss_discriminator', loss_discriminator_train, step = epoch + 1)
 
 			print(f'Segmentation Loss: {loss_segmentation_train}')
 			print(f'Discriminator Loss: {loss_discriminator_train}')
