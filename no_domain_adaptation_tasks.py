9c2e404714a927f1407fe392ae181ad6ffcb7ffb
vinizm <vmartins@poli.ufrj.br>
2022-07-12 14:51:33 -0300

    new scripts

---
+++ no_domain_adaptation_tasks.py
@@ -0,0 +1,14 @@
+from experiments.model_config.no_domain_adaptation import NO_DOMAIN_ADAPTATION_CONFIG, NO_DOMAIN_ADAPTATION_GLOBAL_PARAMS
+from custom_trainer import Trainer
+
+
+for CASE in NO_DOMAIN_ADAPTATION_CONFIG:
+    
+    run_training = CASE.get('run_training')
+    if not run_training:
+        continue  
+    
+    dataset = CASE.get('dataset')
+    patch_size = CASE.get('patch_size', NO_DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patch_size'))
+    
+    print(patch_size)
