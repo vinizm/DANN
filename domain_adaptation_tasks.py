f6707dcbfdb3d79cbccf21ec27cd20a6b8bf5503
Vinicius Martins <vmartins@poli.ufrj.br>
2022-07-17 15:40:35 -0300

    creating domain_adaptation_tasks

---
+++ domain_adaptation_tasks.py
@@ -0,0 +1,87 @@
+from experiment_config.domain_adaptation_config import DOMAIN_ADAPTATION_CONFIG, DOMAIN_ADAPTATION_GLOBAL_PARAMS
+from config import PROCESSED_FOLDER, RESULTS_FOLDER, TEST_INDEX
+from preprocess_images import remove_augmented_images
+from custom_trainer import Trainer
+
+from datetime import datetime
+import tensorflow as tf
+import time
+import os
+import gc
+
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    print('Invalid device or cannot modify virtual devices once initialized.')
+
+now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+EXP_DIR = f'{now}_no_da'
+
+for CASE in DOMAIN_ADAPTATION_CONFIG:
+    
+    run_training = CASE.get('run_training')
+    if not run_training:
+        continue
+    
+    source_set = CASE.get('source')
+    target_set = CASE.get('target')
+    patch_size = CASE.get('patch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patch_size'))
+    stride_train = patch_size // 2
+    channels = CASE.get('channels', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('channels'))
+    num_class = CASE.get('num_class', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_class'))
+    output_stride = CASE.get('output_stride', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('output_stride'))
+    backbone_size = CASE.get('backbone_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('backbone_size'))
+    num_runs = CASE.get('num_runs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_runs'))
+    
+    batch_size = CASE.get('batch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('batch_size'))
+    val_fraction = CASE.get('val_fraction', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('val_fraction'))
+    num_images_train = CASE.get('num_images_train', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_images_train'))
+    rotate = CASE.get('rotate', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('rotate'))
+    flip = CASE.get('flip', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('flip'))
+    max_epochs = CASE.get('max_epochs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('max_epochs'))
+    patience = CASE.get('patience', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patience'))
+    progress_threshold = CASE.get('progress_threshold', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('progress_threshold'))
+    
+    lr_name = CASE.get('lr_name', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_name'))
+    alpha = CASE.get('alpha', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('alpha'))
+    beta = CASE.get('beta', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('beta'))
+    lr0 = CASE.get('lr0', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr0'))
+    lr_warmup = CASE.get('lr_warmup', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_warmup'))
+    
+    PREFIX = f'DL_os{output_stride}_patch{patch_size}_{source_set}_{target_set}'
+    
+    for i in range(num_runs):
+    
+        source_dir = f'{PROCESSED_FOLDER}/{source_set}_patch{patch_size}_stride{stride_train}_Train'
+        target_dir = f'{PROCESSED_FOLDER}/{target_set}_patch{patch_size}_stride{stride_train}_Train'
+        
+        remove_augmented_images(source_dir)
+        remove_augmented_images(target_dir)
+        
+        trainer = Trainer(patch_size = patch_size, channels = channels, num_class = num_class, output_stride = output_stride, backbone_size = backbone_size,
+                          domain_adaptation = True, name = f'{now}_{source_set}_{target_set}_v{i + 1:02}')
+        trainer.set_test_index(test_index_source = TEST_INDEX.get(source_set), test_index_target = TEST_INDEX.get(target_set))
+        trainer.compile_model()
+        trainer.preprocess_images(patches_dir = [source_dir, target_dir], batch_size = batch_size, val_fraction = val_fraction, num_images = num_images_train,
+                                  rotate = rotate, flip = flip)
+        
+        config_segmentation = {'name': lr_name, 'alpha': alpha, 'beta': beta, 'lr0': lr0, 'warmup': lr_warmup}
+        trainer.set_learning_rate(**{'segmentation': config_segmentation})
+        
+        trainer.train_domain_adaptation(epochs = max_epochs, wait = patience, persist_best_model = True, progress_threshold = progress_threshold)
+        
+        LOW_LEVEL_DIR = f'{RESULTS_FOLDER}/{EXP_DIR}/{source_set}_{target_set}/v{i + 1:02}'
+        if not os.path.exists(LOW_LEVEL_DIR):
+            os.makedirs(LOW_LEVEL_DIR)    
+        
+        weights_path = f'{LOW_LEVEL_DIR}/{PREFIX}_v{i + 1:02}_weights.h5'
+        trainer.save_weights(weights_path = weights_path, best = True, piece = None)
+        
+        history_path = f'{LOW_LEVEL_DIR}/{PREFIX}_v{i + 1:02}_history.json'
+        trainer.save_info(history_path = history_path)
+        
+        del trainer
+        gc.collect()
+        
+        time.sleep(15)
--- experiment_config/domain_adaptation_config.py
+++ experiment_config/domain_adaptation_config.py
@@ -1,30 +1,83 @@
-NO_DOMAIN_ADAPTATION_CONFIG = [
+DOMAIN_ADAPTATION_CONFIG = [
+    # Fe19
     {
         'source': 'Fe19',
-        'cases': [{
-            'target': 'Fe120',
-            'num_runs': 5,
-            'run_training': True
-        }]
-    },
-    {
-        'dataset': 'Fe120',
+        'target': 'Fe120',
         'num_runs': 5,
         'run_training': False
     },
     {
-        'dataset': 'FeM',
+        'source': 'Fe19',
+        'target': 'FeM',
         'num_runs': 5,
         'run_training': False
     },
     {
-        'dataset': 'Cu',
+        'source': 'Fe19',
+        'target': 'Cu',
+        'num_runs': 5,
+        'run_training': False
+    },
+    # Fe120
+    {
+        'source': 'Fe120',
+        'target': 'Fe19',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'Fe120',
+        'target': 'FeM',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'Fe120',
+        'target': 'Cu',
+        'num_runs': 5,
+        'run_training': False
+    },
+    # FeM
+    {
+        'source': 'FeM',
+        'target': 'Fe19',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'FeM',
+        'target': 'Fe120',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'FeM',
+        'target': 'Cu',
+        'num_runs': 5,
+        'run_training': False
+    },
+    # Cu
+    {
+        'source': 'Cu',
+        'target': 'Fe19',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'Cu',
+        'target': 'Fe120',
+        'num_runs': 5,
+        'run_training': False
+    },
+    {
+        'source': 'Cu',
+        'target': 'FeM',
         'num_runs': 5,
         'run_training': False
     }
 ]
 
 
-DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'output_stride': 16, 'max_epochs': 150, 'batch_size': 2, 'val_fraction': 0.1,
-                                   'num_images_train': 200, 'patience': 20, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25,
+DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'output_stride': 16, 'max_epochs': 250, 'batch_size': 2, 'val_fraction': 0.1,
+                                   'num_images_train': 200, 'patience': 25, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25,
                                    'beta': 0.75, 'lr0': 5.e-4, 'lr_warmup': 0., 'lr_name': 'exp_decay', 'backbone_size': 16}
