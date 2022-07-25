ae10725423a0b74fe98cbdf48bc3506e7b393a85
Vinicius Martins <vmartins@poli.ufrj.br>
2022-07-24 23:04:01 -0300

    new configs

--- experiment_config/domain_adaptation_config.py
+++ experiment_config/domain_adaptation_config.py
@@ -4,7 +4,7 @@ DOMAIN_ADAPTATION_CONFIG = [
         'source': 'Fe19',
         'target': 'Fe120',
         'num_runs': 5,
-        'run_training': True
+        'run_training': False
     },
     {
         'source': 'Fe19',
@@ -16,7 +16,7 @@ DOMAIN_ADAPTATION_CONFIG = [
         'source': 'Fe19',
         'target': 'Cu',
         'num_runs': 5,
-        'run_training': False
+        'run_training': True
     },
     # Fe120
     {
@@ -78,7 +78,7 @@ DOMAIN_ADAPTATION_CONFIG = [
 ]
 
 
-DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'output_stride': 16, 'max_epochs': 500, 'batch_size': 8, 'val_fraction': 0.1,
-                                   'num_images_train': 200, 'patience': 80, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'backbone_size': 16,
+DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'output_stride': 16, 'max_epochs': 500, 'batch_size': 4, 'val_fraction': 0.1,
+                                   'num_images_train': 200, 'patience': 100, 'flip': True, 'rotate': True, 'progress_threshold': 0.15, 'num_runs': 5, 'backbone_size': 16,
                                    'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4, 'lr_warmup': 0.02, 'lr_name': 'exp_decay', 'gamma': 10., 'lambda_scale': 1.,
                                    'lambda_warmup': 0.02}
