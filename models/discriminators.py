5d813edd3ff503c0b862702e5313b482684ce09a
Vinicius Martins <vmartins@poli.ufrj.br>
2022-08-13 00:23:04 -0300

    convolutional discriminator

--- models/discriminators.py
+++ models/discriminators.py
@@ -3,12 +3,14 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import AveragePooling2D
 
 
-class DomainDiscriminator(Model):
+class DomainDiscriminatorFullyConnected(Model):
 
     def __init__(self, units: int, **kwargs):
-        super(DomainDiscriminator, self).__init__(**kwargs)
+        super(DomainDiscriminatorFullyConnected, self).__init__(**kwargs)
         self.units = units
 
         self.flat = Flatten()
@@ -34,8 +36,83 @@ class DomainDiscriminator(Model):
         return x
 
     def get_config(self):
-        config = super(DomainDiscriminator, self).get_config()
+        config = super(DomainDiscriminatorFullyConnected, self).get_config()
         config.update({'units': self.units})
+        return config
+
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class DomainDiscriminatorLeNet5(Model):
+
+    def __init__(self, **kwargs):
+        super(DomainDiscriminatorLeNet5, self).__init__(**kwargs)
+        
+        self.conv_1 = Conv2D(filters = 192, kernel_size = 5, strides = 1, padding = 'same')
+        self.activ_1 = Activation('relu')
+        
+        self.conv_2 = Conv2D(filters = 128, kernel_size = 5, strides = 1, padding = 'same')
+        self.activ_2 = Activation('relu')
+        
+        self.conv_3 = Conv2D(filters = 128, kernel_size = 5, strides = 1, padding = 'valid')
+        self.activ_3 = Activation('relu')
+        
+        self.avg_pool_1 = AveragePooling2D(pool_size = 2, strides = 2)
+        
+        self.conv_4 = Conv2D(filters = 128, kernel_size = 5, strides = 1)
+        self.activ_4 = Activation('relu')
+        
+        self.avg_pool_2 = AveragePooling2D(pool_size = 2, strides = 2)
+        
+        self.conv_5 = Conv2D(filters = 128, kernel_size = 5, strides = 1)
+        self.activ_5 = Activation('relu')
+        
+        self.flat = Flatten()
+        self.dense_1 = Dense(units = 96)
+        self.activ_6 = Activation('relu')
+        
+        self.dense_2 = Dense(units = 32)
+        self.activ_7 = Activation('relu')        
+        
+        self.dense_3 = Dense(units = 2)
+        self.activ_8 = Activation('softmax')
+
+    def call(self, x):
+        
+        x = self.conv_1(x)
+        x = self.activ_1(x)
+        
+        x = self.conv_2(x)
+        x = self.activ_2(x)
+        
+        x = self.conv_3(x)
+        x = self.activ_3(x)
+        
+        x = self.avg_pool_1(x)
+        
+        x = self.conv_4(x)
+        x = self.activ_4(x)
+        
+        x = self.avg_pool_2(x)
+        
+        x = self.conv_5(x)
+        x = self.activ_5(x)
+        
+        x = self.flat(x)
+        x = self.dense_1(x)
+        x = self.activ_6(x)
+        
+        x = self.dense_2(x)
+        x = self.activ_7(x)
+        
+        x = self.dense_3(x)
+        x = self.activ_8(x)
+
+        return x
+
+    def get_config(self):
+        config = super(DomainDiscriminatorLeNet5, self).get_config()
         return config
 
     def from_config(cls, config):
