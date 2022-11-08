4330840bd8c17c3fa0d354aa3f894da56ad0574d
vinizm <vmartins@poli.ufrj.br>
2022-11-08 13:43:00 -0300

    ew config

--- experiment_config/no_domain_adaptation_config.py
+++ experiment_config/no_domain_adaptation_config.py
@@ -15,16 +15,16 @@ NO_DOMAIN_ADAPTATION_CONFIG = [
     {
         'dataset': 'FeM',
         'num_runs': 3,
-        'run_training': True
+        'run_training': False
     },
     {
         'dataset': 'Cu',
         'num_runs': 3,
-        'run_training': False
+        'run_training': True
     }
 ]
 
 
 NO_DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2,'max_epochs': 120, 'batch_size': 2, 'val_fraction': 0.1, 'num_images_train': 200,
-                                      'patience': 30, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,
+                                      'patience': 20, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,
                                       'lr_warmup': 0., 'lr_name': 'log', 'output_stride': 8, 'lr_log_start': np.log10(5.e-4), 'lr_log_stop': np.log10(5.e-5)}
