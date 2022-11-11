26c01d0b74978550c223daf7886bf4bde13e5d19
vinizm <vmartins@poli.ufrj.br>
2022-11-11 12:50:28 -0300

    fixing deeplab bugs

--- models/deeplabv3plus.py
+++ models/deeplabv3plus.py
@@ -18,161 +18,161 @@ from models.layers import ExpandDimensions
 from models.blocks import AtrousSeparableConv
 
 
-def DeepLabV3Plus(input_shape: tuple = (256, 256, 1), num_class: int = 2, output_stride: int = 8, domain_adaptation: bool = False):
-    
-    if output_stride == 16:
-        side_stride = 2
-        pool = lambda x: MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
-    
-    elif output_stride == 8:
-        side_stride = 1
-        pool = lambda x: x
-        
-    else:
-        return
+def DeepLab(input_shape = (256, 256, 1), num_class = 2, output_stride: int = 8, domain_adaptation: bool = False):
 
-    input_img = Input(shape = input_shape)
+  if output_stride == 16:
+      side_stride = 2
+      pool = lambda x: MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
+  
+  elif output_stride == 8:
+      side_stride = 1
+      pool = lambda x: x
+      
+  else:
+      return
 
-    x = Conv2D(filters = 32, kernel_size = 3, strides = 2, dilation_rate = 1, padding = 'same')(input_img)
-    x = BatchNormalization(epsilon = 1.e-6)(x)
-    x = Activation('relu')(x)
+  input_img = Input(shape = input_shape)
 
-    x = Conv2D(filters = 64, kernel_size = 3, strides = 1, dilation_rate = 1, padding = 'same')(x)
-    x = BatchNormalization(epsilon = 1.e-6)(x)
-    x = Activation('relu')(x)
+  x = Conv2D(filters = 32, kernel_size = 3, strides = 2, dilation_rate = 1, padding = 'same')(input_img)
+  x = BatchNormalization(epsilon = 1.e-6)(x)
+  x = Activation('relu')(x)
 
-    # === ENTRY FLOW 1 ===
+  x = Conv2D(filters = 64, kernel_size = 3, strides = 1, dilation_rate = 1, padding = 'same')(x)
+  x = BatchNormalization(epsilon = 1.e-6)(x)
+  x = Activation('relu')(x)
 
-    residual = Conv2D(filters = 128, kernel_size = 1, strides = 2, dilation_rate = 1, padding = 'valid')(x)
-    residual = BatchNormalization(epsilon = 1.e-6)(residual)
+  # === ENTRY FLOW 1 ===
 
-    x = AtrousSeparableConv(x, filters = 128, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-    x = AtrousSeparableConv(x, filters = 128, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
-    x = MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
+  residual = Conv2D(filters = 128, kernel_size = 1, strides = 2, dilation_rate = 1, padding = 'valid')(x)
+  residual = BatchNormalization(epsilon = 1.e-6)(residual)
 
-    x = Add()([x, residual])
+  x = AtrousSeparableConv(x, filters = 128, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  x = AtrousSeparableConv(x, filters = 128, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
+  x = MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
 
-    # === ENTRY FLOW 2 ===
+  x = Add()([x, residual])
 
-    residual = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(residual)
-    residual = BatchNormalization(epsilon = 1.e-6)(residual)
+  # === ENTRY FLOW 2 ===
 
-    x = Activation('relu')(x)
-    x = AtrousSeparableConv(x, filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-    x = AtrousSeparableConv(x, filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
-    # x = MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
+  residual = Conv2D(filters = 256, kernel_size = 1, strides = 2, dilation_rate = 1, padding = 'valid')(residual)
+  residual = BatchNormalization(epsilon = 1.e-6)(residual)
 
-    x = Add()([x, residual])
-    x = Dropout(0.2)(x) # Dropout
+  x = Activation('relu')(x)
+  x = AtrousSeparableConv(x, filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  x = AtrousSeparableConv(x, filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
+  x = MaxPool2D(pool_size = 2, strides = 2, padding = 'valid')(x)
 
-    # === ENTRY FLOW 3 ===
+  x = Add()([x, residual])
+  x = Dropout(0.2)(x) # Dropout
 
-    residual = Conv2D(filters = 728, kernel_size = 1, strides = side_stride, dilation_rate = 1, padding = 'valid')(residual)
-    residual = BatchNormalization(epsilon = 1.e-6)(residual)
+  # === ENTRY FLOW 3 ===
 
-    x = Activation('relu')(x)
-    x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-    skip_conn = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
-    x = pool(skip_conn)
+  residual = Conv2D(filters = 728, kernel_size = 1, strides = side_stride, dilation_rate = 1, padding = 'valid')(residual)
+  residual = BatchNormalization(epsilon = 1.e-6)(residual)
 
-    bypass = Add()([x, residual])
-    x = bypass
+  x = Activation('relu')(x)
+  x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  skip_conn = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
+  x = pool(skip_conn)
 
-    # ==== MIDDLE FLOW ====
+  bypass = Add()([x, residual])
+  x = bypass
 
-    for _ in range(8):
-        x = Activation('relu')(x)
-        x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-        x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-        x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  # ==== MIDDLE FLOW ====
 
-    x = Add()([x, bypass])
-    x = Activation('relu')(x)
-    x = Dropout(0.2)(x) # Dropout
+  for _ in range(8):
+      x = Activation('relu')(x)
+      x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+      x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+      x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
 
-    # === EXIT FLOW 1 ===
+  x = Add()([x, bypass])
+  x = Activation('relu')(x)
+  x = Dropout(0.2)(x) # Dropout
 
-    residual = Conv2D(filters = 1024, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
-    residual = BatchNormalization(epsilon = 1.e-6)(residual)
+  # === EXIT FLOW 1 ===
 
-    x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-    x = AtrousSeparableConv(x, filters = 1024, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
+  residual = Conv2D(filters = 1024, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  residual = BatchNormalization(epsilon = 1.e-6)(residual)
 
-    x = Add()([x, residual])
-    x = Dropout(0.2)(x) # Dropout
+  x = AtrousSeparableConv(x, filters = 728, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  x = AtrousSeparableConv(x, filters = 1024, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
 
-    # === EXIT FLOW 2 ===
+  x = Add()([x, residual])
+  x = Dropout(0.2)(x) # Dropout
 
-    x = AtrousSeparableConv(x, filters = 1536, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
-    x = AtrousSeparableConv(x, filters = 2048, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
-    x = Dropout(0.2)(x) # Dropout
+  # === EXIT FLOW 2 ===
 
-    # === IMAGE LEVEL FEATURES ===
+  x = AtrousSeparableConv(x, filters = 1536, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = True)
+  x = AtrousSeparableConv(x, filters = 2048, kernel_size = 3, strides = 1, dilation_rate = 1, batch_norm = True, relu_activation = False)
+  x = Dropout(0.2)(x) # Dropout
 
-    default_shape = tuple(x.shape)
-    b0 = GlobalAveragePooling2D()(x)
+  # === IMAGE LEVEL FEATURES ===
 
-    # (batch_size, channels) -> (batch_size, 1, 1, channels)
-    b0 = ExpandDimensions(axis = 1)(b0)
-    b0 = ExpandDimensions(axis = 1)(b0)
+  default_shape = tuple(x.shape)
 
-    b0 = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(b0)
+  b0 = GlobalAveragePooling2D()(x)
 
-    previous_shape = tuple(b0.shape)
-    upsampling_factor = int(default_shape[1] / previous_shape[1])
+  # (batch_size, channels) -> (batch_size, 1, 1, channels)
+  b0 = ExpandDimensions(axis = 1)(b0)
+  b0 = ExpandDimensions(axis = 1)(b0)
 
-    b0 = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(b0)
+  b0 = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(b0)
 
-    # ====== ASPP ======
+  previous_shape = tuple(b0.shape)
+  upsampling_factor = int(default_shape[1] / previous_shape[1])
 
-    b1 = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
-    b2 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, padding = 'same')(x)
-    b3 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 2, padding = 'same')(x)
-    b4 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 3, padding = 'same')(x)
+  b0 = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(b0)
 
-    x = Concatenate()([b0, b1, b2, b3, b4])
-    features = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
-    print(f'Bottleneck: {features.shape}')
+  # ====== ASPP ======
 
-    # ====== DECODER ======
+  b1 = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  b2 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 1, padding = 'same')(x)
+  b3 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 2, padding = 'same')(x)
+  b4 = Conv2D(filters = 256, kernel_size = 3, strides = 1, dilation_rate = 3, padding = 'same')(x)
 
-    skip_conn = Conv2D(filters = 48, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(skip_conn)
+  x = Concatenate()([b0, b1, b2, b3, b4])
+  features = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  print(f'Bottleneck: {features.shape}')
 
-    previous_shape = tuple(skip_conn.shape)
-    target_dim = input_shape[1] / 2
-    upsampling_factor = int(target_dim / previous_shape[1])
-    print(f'Upsampling Skip Conn: {upsampling_factor}')
+  # ====== DECODER ======
 
-    skip_conn = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(skip_conn)
+  skip_conn = Conv2D(filters = 48, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(skip_conn)
+  
+  previous_shape = tuple(skip_conn.shape)
+  target_dim = input_shape[1] / 2
+  upsampling_factor = int(target_dim / previous_shape[1])
+  print(f'Upsampling Skip Conn: {upsampling_factor}')
 
-    previous_shape = tuple(features.shape)
-    upsampling_factor = int(target_dim / previous_shape[1])
-    print(f'Upsampling Features: {upsampling_factor}')
+  skip_conn = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(skip_conn)
 
-    x = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(features)
-    print(f'Concatenate: {x.shape, skip_conn.shape}')
-    x = Concatenate()([x, skip_conn])
+  previous_shape = tuple(features.shape)
+  upsampling_factor = int(target_dim / previous_shape[1])
+  print(f'Upsampling Features: {upsampling_factor}')
 
-    x = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
-    x = BatchNormalization(epsilon = 1.e-6)(x)
-    x = Activation('relu')(x)
+  x = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(features)
+  print(f'Concatenate: {x.shape, skip_conn.shape}')
+  x = Concatenate()([x, skip_conn])
 
-    x = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
-    x = BatchNormalization(epsilon = 1.e-6)(x)
-    x = Activation('relu')(x)
+  x = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  x = BatchNormalization(epsilon = 1.e-6)(x)
+  x = Activation('relu')(x)
 
-    x = Conv2D(filters = num_class, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  x = Conv2D(filters = 256, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
+  x = BatchNormalization(epsilon = 1.e-6)(x)
+  x = Activation('relu')(x)
 
-    original_shape = tuple(input_img.shape)
-    previous_shape = tuple(x.shape)
-    upsampling_factor = int(original_shape[1] / previous_shape[1])
-    print(f'Final Upsampling: {upsampling_factor}')
+  x = Conv2D(filters = num_class, kernel_size = 1, strides = 1, dilation_rate = 1, padding = 'valid')(x)
 
-    x = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(x)
-    output_proba = Activation('softmax')(x)
-    
-    if domain_adaptation:
-        output_proba = [output_proba, features]
+  previous_shape = tuple(x.shape)
+  upsampling_factor = int(input_shape[1] / previous_shape[1])
+  print(f'Final Upsampling: {upsampling_factor}')
 
-    model = Model(inputs = input_img, outputs = output_proba)
-    return model
+  x = UpSampling2D(size = upsampling_factor, interpolation = 'bilinear')(x)
+  output_proba = Activation('softmax')(x)
+
+  if domain_adaptation:
+      output_proba = [output_proba, features]
+
+  model = Model(inputs = input_img, outputs = output_proba)
+  return model
