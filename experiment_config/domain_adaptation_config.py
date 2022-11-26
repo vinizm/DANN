a59c3b30a75cf1efe5219af435790596bd2a2499
vinizm <vmartins@poli.ufrj.br>
2022-11-26 14:33:13 -0300

    new configs

--- experiment_config/domain_adaptation_config.py
+++ experiment_config/domain_adaptation_config.py
@@ -81,7 +81,6 @@ DOMAIN_ADAPTATION_CONFIG = [
 ]
 
 
-DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'max_epochs': 120, 'batch_size': 4, 'val_fraction': 0.1,
-                                   'num_images_train': 200, 'patience': 100, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5,
-                                   'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,'lr_warmup': 0., 'lr_name': 'log', 'output_stride': 8, 'lr_log_start': np.log10(5.e-4),
-                                   'lr_log_stop': np.log10(5.e-5)}
+DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'max_epochs': 120, 'batch_size': 4, 'val_fraction': 0.1, 'num_images_train': 200,
+                                   'patience': 100, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,
+                                   'lr_warmup': 0., 'lr_name': 'log', 'output_stride': 8, 'lr_log_start': np.log10(5.e-4), 'lr_log_stop': np.log10(5.e-5)}
