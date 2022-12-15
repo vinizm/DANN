93aef29738509a5202078a1676b0945fae748882
vinizm <vmartins@poli.ufrj.br>
2022-12-15 19:14:52 -0300

    config

--- experiment_config/domain_adaptation_config.py
+++ experiment_config/domain_adaptation_config.py
@@ -32,7 +32,7 @@ DOMAIN_ADAPTATION_CONFIG = [
         'source': 'Fe120',
         'target': 'FeM',
         'num_runs': 5,
-        'run_training': True
+        'run_training': False
     },
     {
         'source': 'Fe120',
@@ -69,8 +69,8 @@ DOMAIN_ADAPTATION_CONFIG = [
     {
         'source': 'Cu',
         'target': 'Fe120',
-        'num_runs': 5,
-        'run_training': False
+        'num_runs': 1,
+        'run_training': True
     },
     {
         'source': 'Cu',
@@ -81,7 +81,7 @@ DOMAIN_ADAPTATION_CONFIG = [
 ]
 
 
-DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'max_epochs': 50, 'batch_size': 4, 'val_fraction': 0.1, 'num_images_train': 200,
-                                   'patience': 120, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,
-                                   'lr_warmup': 0., 'lr_name': 'log', 'output_stride': 16, 'lr_log_start': np.log10(1.e-4), 'lr_log_stop': np.log10(1.e-5),
-                                   'lambda_scale': 1., 'gamma': 5., 'lambda_warmup': 0.}
+DOMAIN_ADAPTATION_GLOBAL_PARAMS = {'patch_size': 256, 'channels': 1, 'num_class': 2, 'max_epochs': 1000, 'batch_size': 4, 'val_fraction': 0.1, 'num_images_train': 200,
+                                   'patience': None, 'flip': True, 'rotate': True, 'progress_threshold': 0., 'num_runs': 5, 'alpha': 2.25, 'beta': 0.75, 'lr0': 5.e-4,
+                                   'lr_warmup': 0.01, 'lr_name': 'log', 'output_stride': 16, 'lr_log_start': np.log10(2.e-4), 'lr_log_stop': np.log10(1.e-5),
+                                   'lambda_scale': 1., 'gamma': 10., 'lambda_warmup': 0.01}
