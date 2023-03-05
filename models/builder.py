26ef15d38495a93d1cdcd84792b7e0c551580c14
vinizm <vmartins@poli.ufrj.br>
2023-03-05 16:36:30 -0300

    new structure for deeplab

--- models/builder.py
+++ models/builder.py
@@ -2,17 +2,17 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 
-from models.deeplabv3plus import deeplabv3plus
+from models.deeplabv3plus import DeepLabV3Plus
 from models.layers import GradientReversalLayer
 from models.discriminators import DomainDiscriminatorFullyConnected, DomainDiscriminatorPixelwise
 
 
 class DomainAdaptationModel(Model):
 
-    def __init__(self, input_shape: tuple = (256, 256, 1), num_class: int = 2, output_stride: int = 8, skip_conn: bool = True, **kwargs):
+    def __init__(self, input_shape: tuple = (256, 256, 1), num_class: int = 2, output_stride: int = 8, **kwargs):
         super(DomainAdaptationModel, self).__init__(**kwargs)
 
-        self.main_network = deeplabv3plus(input_shape = input_shape, num_class = num_class, output_stride = output_stride, skip_conn = skip_conn, domain_adaptation = True)
+        self.main_network = DeepLabV3Plus(input_shape = input_shape, num_class = num_class, output_stride = output_stride)
         self.gradient_reversal_layer = GradientReversalLayer()
         self.domain_discriminator = DomainDiscriminatorPixelwise(zero_mean = False)
 
@@ -24,8 +24,10 @@ class DomainAdaptationModel(Model):
     def call(self, inputs):
         input_img, l = inputs
 
-        segmentation_output, feature_output = self.main_network(input_img)
-        discriminator_input = self.gradient_reversal_layer([feature_output, l])
+        bottleneck_features, skip_connect = self.main_network.encoder(input_img)
+        segmentation_output = self.main_network.decoder([bottleneck_features, skip_connect])
+        
+        discriminator_input = self.gradient_reversal_layer([bottleneck_features, l])
         discriminator_output = self.domain_discriminator(discriminator_input)
 
         return segmentation_output, discriminator_output
