8bf7650895463387a4180f316a1d67a07ba39dcf
vinizm <vmartins@poli.ufrj.br>
2023-03-05 15:09:36 -0300

    no major changes

--- models/deeplabv3plus.py
+++ models/deeplabv3plus.py
@@ -194,8 +194,8 @@ class DeepLabV3Plus(Model):
         
         self.encoder = deeplabv3plus_encoder(input_shape = input_shape, output_stride = output_stride)
         
-        features_shape = tuple(self.encoder.outputs[0].shape[1 :])
-        skip_connect_shape = tuple(self.encoder.outputs[1].shape[1 :])
+        features_shape = tuple(self.encoder.outputs[0].shape[1:])
+        skip_connect_shape = tuple(self.encoder.outputs[1].shape[1:])
         self.decoder = deeplabv3plus_decoder(input_shape = input_shape, features_shape = features_shape, num_class = num_class, skip_connect_shape = skip_connect_shape)
         
         self.inputs = Input(shape = input_shape)
