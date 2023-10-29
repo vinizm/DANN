b2db81b9b9eb9871b1524e509bc1c3cc5848d521
vinizm <vzmartinsds@gmail.com>
2023-10-29 18:22:24 -0300

    grid search function

--- domain_adaptation_tasks.py
+++ domain_adaptation_tasks.py
@@ -18,42 +18,34 @@ try:
 except:
     print('Invalid device or cannot modify virtual devices once initialized.')
 
-now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-EXP_DIR = f'{now}_da'
 
-for CASE in DOMAIN_ADAPTATION_CONFIG:
-    
-    run_training = CASE.get('run_training')
-    if not run_training:
-        continue
-    
-    source_set = CASE.get('source')
-    target_set = CASE.get('target')
-    patch_size = CASE.get('patch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patch_size'))
-    stride_train = patch_size // 2
-    channels = CASE.get('channels', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('channels'))
-    num_class = CASE.get('num_class', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_class'))
-    output_stride = CASE.get('output_stride', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('output_stride'))
-    skip_conn = CASE.get('skip_conn', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('skip_conn'))
-    units = CASE.get('units', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('units'))
-    num_runs = CASE.get('num_runs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_runs'))
-    
-    batch_size = CASE.get('batch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('batch_size'))
-    val_fraction = CASE.get('val_fraction', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('val_fraction'))
-    num_images_train = CASE.get('num_images_train', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_images_train'))
-    rotate = CASE.get('rotate', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('rotate'))
-    flip = CASE.get('flip', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('flip'))
-    max_epochs = CASE.get('max_epochs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('max_epochs'))
-    patience = CASE.get('patience', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patience'))
-    progress_threshold = CASE.get('progress_threshold', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('progress_threshold'))
-    
-    lr_config_main = CASE.get('lr_config_main', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_config_main'))
-    lr_config_extra = CASE.get('lr_config_extra', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_config_extra'))
-    optimizer_config = CASE.get('optimizer_config', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('optimizer_config'))
-    
-    gamma = CASE.get('gamma', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('gamma'))
-    lambda_scale = CASE.get('lambda_scale', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lambda_scale'))
-    lambda_warmup = CASE.get('lambda_warmup', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lambda_warmup'))
+def grid_search(
+    source_set: str,
+    target_set: str,
+    patch_size: int,
+    stride_train: int,
+    channels: int,
+    num_class: int,
+    output_stride: int,
+    skip_conn: bool,
+    units: int,
+    num_runs: int,
+    batch_size: int,
+    val_fraction: float,
+    num_images_train: int,
+    rotate: bool,
+    flip: bool,
+    max_epochs: int,
+    patience: int,
+    progress_threshold: float,
+    lr_config_main: dict,
+    lr_config_extra: dict,
+    optimizer_config: dict,
+    gamma: float,
+    lambda_scale: float,
+    lambda_warmup: float,
+    exp_dir: str
+):
     
     PREFIX = f'DL_patch{patch_size}_{source_set}_{target_set}'
     
@@ -105,7 +97,7 @@ for CASE in DOMAIN_ADAPTATION_CONFIG:
         
         trainer.train_domain_adaptation(epochs = max_epochs, wait = patience, persist_best_model = True, progress_threshold = progress_threshold)
 
-        LOW_LEVEL_DIR = f'{RESULTS_FOLDER}/{EXP_DIR}/{source_set}_{target_set}/v{i + 1:02}'
+        LOW_LEVEL_DIR = f'{RESULTS_FOLDER}/{exp_dir}/{source_set}_{target_set}/v{i + 1:02}'
         if not os.path.exists(LOW_LEVEL_DIR):
             os.makedirs(LOW_LEVEL_DIR)
         
@@ -122,3 +114,79 @@ for CASE in DOMAIN_ADAPTATION_CONFIG:
         gc.collect()
         
         time.sleep(15)
+        
+    return True
+
+
+def convert_to_iterable(x):
+    if isinstance(x, list) or isinstance(x, tuple):
+        return x
+    
+    else:
+        return [x]
+
+
+now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+EXP_DIR = f'{now}_da'
+
+for CASE in DOMAIN_ADAPTATION_CONFIG:
+    
+    run_training = CASE.get('run_training')
+    if not run_training:
+        continue
+    
+    source_set = CASE.get('source')
+    target_set = CASE.get('target')
+    patch_size = CASE.get('patch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patch_size'))
+    stride_train = patch_size // 2
+    channels = CASE.get('channels', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('channels'))
+    num_class = CASE.get('num_class', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_class'))
+    output_stride = CASE.get('output_stride', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('output_stride'))
+    skip_conn = CASE.get('skip_conn', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('skip_conn'))
+    units = CASE.get('units', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('units'))
+    num_runs = CASE.get('num_runs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_runs'))
+    
+    batch_size = CASE.get('batch_size', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('batch_size'))
+    val_fraction = CASE.get('val_fraction', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('val_fraction'))
+    num_images_train = CASE.get('num_images_train', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('num_images_train'))
+    rotate = CASE.get('rotate', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('rotate'))
+    flip = CASE.get('flip', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('flip'))
+    max_epochs = CASE.get('max_epochs', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('max_epochs'))
+    patience = CASE.get('patience', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('patience'))
+    progress_threshold = CASE.get('progress_threshold', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('progress_threshold'))
+    
+    lr_config_main = CASE.get('lr_config_main', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_config_main'))
+    lr_config_extra = CASE.get('lr_config_extra', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lr_config_extra'))
+    optimizer_config = CASE.get('optimizer_config', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('optimizer_config'))
+    
+    gamma = CASE.get('gamma', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('gamma'))
+    lambda_scale = CASE.get('lambda_scale', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lambda_scale'))
+    lambda_warmup = CASE.get('lambda_warmup', DOMAIN_ADAPTATION_GLOBAL_PARAMS.get('lambda_warmup'))
+
+    grid_search(
+        source_set = source_set,
+        target_set = target_set,
+        patch_size = patch_size,
+        stride_train = stride_train,
+        channels = channels,
+        num_class = num_class,
+        output_stride = output_stride,
+        skip_conn = skip_conn,
+        units = units,
+        num_runs = num_runs,
+        batch_size = batch_size,
+        val_fraction = val_fraction,
+        num_images_train = num_images_train,
+        rotate = rotate,
+        flip = flip,
+        max_epochs = max_epochs,
+        patience = patience,
+        progress_threshold = progress_threshold,
+        lr_config_main = lr_config_main,
+        lr_config_extra = lr_config_extra,
+        optimizer_config = optimizer_config,
+        gamma = gamma,
+        lambda_scale = lambda_scale,
+        lambda_warmup = lambda_warmup,
+        exp_dir = EXP_DIR
+    )
